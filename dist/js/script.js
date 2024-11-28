c9c55e79d3823e9e6fbe35821d2c929182a491c0
mushaf35 <mushafmiftah35@gmail.com>
2024-11-28 12:38:23 +0700

    Perbaikan pada bagian sticky menu, tombol back to top & dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,17 @@
 window.onscroll = function() {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
-  if(window.scrollY > fixedNav){
+  if(window.scrollY > fixedNav) {
+  // if(window.scrollY > fixedNav){
     header.classList.add ('navbar-fixed');
-} else {
+    toTop.classList.remove ('hidden');
+    toTop.classList.add ('flex');
+  } else {
     header.classList.remove ('navbar-fixed');
+    toTop.classList.remove ('flex');
+    toTop.classList.add ('hidden');
 }
 };
 
@@ -18,3 +24,21 @@ hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// Klik di luar hamburger
+
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('#hamburger') && !event.target.matches('.nav-link')) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// Dark mode
+const checkbox = document.querySelector('#toggle');
+        const html = document.querySelector('html');
+
+        checkbox.addEventListener('click', function()
+        {
+            checkbox.checked ? html.classList.add('dark') : html.classList.remove('dark');
+        });
